3ea35128c52b400b24eca97c96e7f23bb6368132
unknown <danieleastwood4@gmail.com>
2022-07-15 00:18:45 -0400

    Products/Homescreen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import React from "react";
+import Header1 from "./components/Header1";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Nxt Purchase</h1>
+      <Header1 />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
